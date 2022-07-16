6eff2d468343d7f78e3110177ae84d1ed78dfe1f
Nyamekesse Samuel <snyamekesse@stu.ucc.edu.gh>
2022-07-16 23:06:30 +0000

    added components files

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#666",
+    },
+    secondary: {
+      main: "#111",
+    },
+  },
+});
